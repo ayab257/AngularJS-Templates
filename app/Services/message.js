5acd13e81fc78843dddafcdb3228ebb7ed707303
ayab257 <ayab257@gmail.com>
2016-09-14 14:36:20 +0300

    trying

---
+++ app/Services/message.js
@@ -0,0 +1,14 @@
+(function() {
+	var app = angular.module("imdb")
+	app.factory("message", message)
+
+	function message() {
+		
+		return {
+			print : function(text) {
+				alert("hello " + text)
+			}
+		};
+	}
+
+})();
