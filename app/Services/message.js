f48db5017d02d8579503a80cb74397cdbea21cb8
ayab257 <ayab257@gmail.com>
2016-09-15 22:55:09 +0300

    showing img of actor

--- app/Services/message.js
+++ app/Services/message.js
@@ -1,12 +1,12 @@
 (function() {
-	var app = angular.module("imdb")
-	app.factory("message", message)
+	var app = angular.module("imdb");
+	app.factory("message", message);
 
 	function message() {
 		
 		return {
 			print : function(text) {
-				alert("hello " + text)
+				alert("hello " + text);
 			}
 		};
 	}
---
+++ app/controllers/img.js
@@ -0,0 +1,15 @@
+// (function() {
+// 
+    // var img = angular.module('imdb');
+// 
+    // img.controller('img' ,imgFunction);
+// 
+    // function imgFunction($scope, $location) {
+// 
+// 
+        // $scope.profile_path = $location.search();
+// 
+        // $scope.url = 'http://image.tmdb.org/t/p/w500'+$scope.profile_path;
+    // }
+// 
+// })();
